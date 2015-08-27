f55886ce03e1ec39dcd3bbce13ae6c3b5c287da8
roammi <ramirabal@gmail.com>
2015-08-26 23:50:23 -0400

    commit 3, es necesario pip install README

--- integraTuSalud/home/models.py
+++ integraTuSalud/home/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+#from django import forms
+from tinymce import models as tinymce_models
+# from mongoengine import *
 # Create your models here.
+ELECCIONES_CATEGORIA = (
+	('MED', "MEDICINA"),
+	('FIS', 'FISIOTERAPIA'),
+	('ODO', 'ODONTOLOGIA'),
+	('EST', 'ESTUDIANTIL'),
+)
+
+class Articulo(models.Model):
+	"""docstring foUserme"""
+	titulo = models.CharField(max_length=250)#StringField(max_length=200)
+	autor = models.CharField(max_length=200)
+	contenido = tinymce_models.HTMLField()
+	imagen = models.ImageField(upload_to = 'static/img/')
+	etiqueta = models.CharField(max_length=3, choices=ELECCIONES_CATEGORIA, default='MED')
+	fecha = models.DateTimeField('fecha de publicacion')
+	#def __init__(self, arg):
+	#	superUsere, self).__init__()
+	#	self.arg = arg
